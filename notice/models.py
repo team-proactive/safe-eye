d1dfa67241050eff6fa67449f6f349ac6ba6d3f4
HY. Lee <89088205+matty255@users.noreply.github.com>
2024-04-01 15:44:38 +0900

    ✨🐛 [notice] (#9) (#8) 디펜던시 추가 및 모델 프로퍼티 추가
    * 🐛 fix: environ 디펜던시 추가
    
    * ✨ feat: notice 모델에 id 프로퍼티 추가

--- notice/models.py
+++ notice/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Notice(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
