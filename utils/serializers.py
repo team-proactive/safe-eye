d6a8f6a14c262c6cd56384d3d539d76603e60d51
mkdirlife <mkdirlife@naver.com>
2024-04-07 18:47:28 +0900

    ✅🎨 serializers.py, tests.py 추가 및 DRF 적용 코드로 수정

---
+++ utils/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Tag, Status
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'tag_type', 'tag_content', 'tag_id', 'content_type', 'object_id']
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = ['id', 'available', 'content_type', 'object_id', 'created_at', 'updated_at']
