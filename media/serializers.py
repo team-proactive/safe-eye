ded46076cc04e866183891995a1c8cef5d3537c7
rkawkclzls <kslee1761@gmail.com>
2024-04-10 02:10:01 +0900

    djangorestframework-simplejwt설치

--- media/serializers.py
+++ media/serializers.py
@@ -1,7 +1,4 @@
-from rest_framework import serializers
-from .models import MediaFile
-
 class MediaFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = MediaFile
-        fields = ['id', 'file', 'created_at', 'description', 'file_size', 'file_type']
+        fields = ['id', 'file', 'created_at', 'description', 'file_size', 'file_type', 'ai_model']
