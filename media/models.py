9842647b1fe5bd8a515f63eaed52c919f29e0ad3
rkawkclzls <kslee1761@gmail.com>
2024-04-10 01:51:26 +0900

    AI모델 파일의 경로를 저장할 필드 추가

--- media/models.py
+++ media/models.py
@@ -8,6 +8,9 @@ class MediaFile(models.Model):
     description = models.TextField(blank=True)
     file_size = models.BigIntegerField(null=True, blank=True)
     file_type = models.CharField(max_length=100, null=True, blank=True)
+    class MediaFile(models.Model):
+    # ... 기존 필드들 ...
+    ai_model = models.FileField(upload_to='ai_models/', blank=True, null=True)
 
 #위 예시에서는 description 필드를 추가하여 파일에 대한 설명을 저장할 수 있도록 하였고, 
 # file_size 필드를 추가하여 파일 크기를 저장할 수 있도록 하였습니다. 
