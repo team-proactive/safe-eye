cdac66926214480ffb00b129a4c5641830d5e9cb
HY. Lee <89088205+matty255@users.noreply.github.com>
2024-04-10 23:22:13 +0900

    [accounts] (#42) (#35) 로그인 기능 추가

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,20 +1,35 @@
+# accounts/serializers.py
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+
 
 class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['email', 'password', 'nickname']
+        fields = ["email", "password", "nickname"]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
-            email=validated_data['email'],
-            password=validated_data['password'],
-            nickname=validated_data['nickname']
+            email=validated_data["email"],
+            password=validated_data["password"],
+            nickname=validated_data["nickname"],
         )
         return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return {"user": user}
+        raise serializers.ValidationError("Incorrect Credentials")
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['email', 'nickname']
+        fields = ["email", "nickname"]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,20 @@
+# accounts/views.py
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.decorators import action
 from .models import CustomUser
-from .serializers import CustomUserSerializer, UserCreateSerializer
+from .serializers import CustomUserSerializer, UserCreateSerializer, UserLoginSerializer
+
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
 
     def get_serializer_class(self):
-        if self.action in ['create', 'update']:
+        if self.action == "create":
             return UserCreateSerializer
+        elif self.action == "login":
+            return UserLoginSerializer
         return CustomUserSerializer
 
     def create(self, request, *args, **kwargs):
@@ -17,21 +22,32 @@ class CustomUserViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         user = self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-
-        refresh = RefreshToken.for_user(user)
-        token = str(refresh.access_token)
-
-        return Response({'token': token, 'user': serializer.data}, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
     def perform_create(self, serializer):
         return serializer.save()
+
+    @action(detail=False, methods=["post"])
+    def login(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        refresh = RefreshToken.for_user(user)
+        return Response(
+            {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "user": CustomUserSerializer(user).data,
+            }
+        )
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
-
         return Response(serializer.data)
 
     def perform_update(self, serializer):
