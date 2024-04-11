1c3a01d5f4ce1a4cf136658028d5573a0706f145
najasinis <tttjjjyyy1234@daum.net>
2024-04-11 10:18:41 +0900

    [accounts_admin 수정] feat :'유저 추가 및 runserver 확인 완료'

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "email",
+        "is_staff",
+        "is_superuser",
+    )  # 'username' 대신 실제 필드 이름 사용
+    ordering = ("email",)  # 'username' 대신 실제 필드 이름 사용
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
