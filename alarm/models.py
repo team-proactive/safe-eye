3214be32174452b33c909692faa6de336cba2d01
mkdirlife <78574080+mkdirlife@users.noreply.github.com>
2024-04-13 13:47:00 +0900

    [alarm] [utils] (#53) (#52) alarm 앱 고도화 작업, utils 앱 mixins.py 수정
    * ✅ alarm 앱 고도화
    
    * 🐛 utils앱 mixins.py IsAuthorOrReadOnly 오류 수정

--- alarm/models.py
+++ alarm/models.py
@@ -3,26 +3,42 @@ from django.conf import settings
 from utils.models import TagMixin, StatusMixin
 
 
-class Alarm(TagMixin, StatusMixin, models.Model):
-    ALARM_TYPES = (
-        ("FD", "전도"),
-        ("BR", "파손"),
-        ("AR", "방화"),
-        ("SM", "흡연"),
-        ("AB", "유기"),
-        ("TH", "절도"),
-        ("AS", "폭행"),
-        ("TV", "교통약자"),
+class AlarmType(models.Model):
+    code = models.CharField(max_length=2, unique=True)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Risk(models.Model):
+    RISK_LEVELS = (
+        ("VL", "매우 낮음"),
+        ("LO", "낮음"),
+        ("ME", "보통"),
+        ("HI", "높음"),
+        ("VH", "매우 높음"),
     )
 
+    level = models.CharField(max_length=2, choices=RISK_LEVELS)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.get_level_display()
+
+
+class Alarm(TagMixin, StatusMixin, models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="alarms"
     )
-    alarm_type = models.CharField(max_length=2, choices=ALARM_TYPES)
+    camera_id = models.IntegerField(default=1)
+    alarm_type = models.ForeignKey(AlarmType, on_delete=models.CASCADE)
     alarm_content = models.TextField()
+    risk = models.ForeignKey(Risk, on_delete=models.CASCADE, default=1)
+    custom_message = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.get_alarm_type_display()} - {self.alarm_content[:20]}..."
+        return f"{self.alarm_type.name} - {self.alarm_content[:20]}..."
