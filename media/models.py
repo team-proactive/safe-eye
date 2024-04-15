cb1b51e74aee3d305da29587803a43aedaf16f8b
rkawkclzls <kslee1761@gmail.com>
2024-04-15 23:09:48 +0900

    models.py수정

--- media/models.py
+++ media/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 
-
 class MediaFile(models.Model):
     app_label = 'media'
-    file = models.FileField(upload_to='media/')
+    video_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True)
-    file_size = models.BigIntegerField(null=True, blank=True)
-    file_type = models.CharField(max_length=100, null=True, blank=True)
-    ai_model = models.FileField(upload_to='ai_models/', blank=True, null=True)
-
-#위 예시에서는 description 필드를 추가하여 파일에 대한 설명을 저장할 수 있도록 하였고, 
-# file_size 필드를 추가하여 파일 크기를 저장할 수 있도록 하였습니다. 
-# 또한 file_type 필드를 추가하여 파일의 형식을 저장할 수 있도록 하였습니다.
+    result = models.JSONField(null=True, blank=True)
