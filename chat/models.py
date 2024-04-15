01967adb959ddd4a7abc3c9ea546b7a41fbeeb63
najasinis <tttjjjyyy1234@daum.net>
2024-04-15 14:52:14 +0900

    🐛feat: fix chat test.py code

--- chat/models.py
+++ chat/models.py
@@ -6,12 +6,16 @@ User = get_user_model()
 
 class ChatRoom(models.Model):
     name = models.CharField(max_length=255)
-    users = models.ManyToManyField(User)
+    users = models.ManyToManyField(User, related_name="chat_rooms")
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Message(models.Model):
-    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(
+        ChatRoom, on_delete=models.CASCADE, related_name="messages"
+    )
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_messages"
+    )
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
